26f630ae243b1661d928eef98b3ca0b1e0152426
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-18 18:39:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,12 @@ class TicTacToe
   [1,4,7],
   [2,5,8]
 ]
+
+def display_board
+  puts " #{@board[0]} " + "|" + " #{@board[1]} " + "|" + " #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} " + "|" + " #{@board[4]} " + "|" + " #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
+end
 end
