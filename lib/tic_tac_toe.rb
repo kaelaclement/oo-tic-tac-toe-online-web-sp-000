daf597140f3648de31d80851e17c2994128c40dc
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-18 19:26:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,4 +108,15 @@ class TicTacToe
       token
     end
   end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 end
