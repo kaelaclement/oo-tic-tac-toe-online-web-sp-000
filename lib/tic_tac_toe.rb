e9c55e3f0049da5d6bd155c67b25dda384b526a3
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-18 18:40:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,4 +21,9 @@ def display_board
   puts "-----------"
   puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
 end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
 end
