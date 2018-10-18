8c64e12ec9cb211056057564ca6f0460ddf01e47
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-18 19:12:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,8 @@ class TicTacToe
     index = input.to_i - 1
   end
 
-  def move (index, token = "X")
-    @board[index] = token
+  def move (index, current_player = "X")
+    @board[index] = current_player
   end
 
   def position_taken?(index)
