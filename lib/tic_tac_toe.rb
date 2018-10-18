c967960a959a6483ddd3ec6373b361a70cdcfe14
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-18 19:13:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,4 +66,17 @@ class TicTacToe
       turn
     end
   end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_array|
+      if win_array.all?{|board_position| board[board_position] == "X"}
+        return win_array
+      elsif win_array.all?{|board_position| board[board_position] == "O"}
+        return win_array
+      end
+    end
+    if board.all?{|space| position_taken?(board, space.to_i)} || board.none?{|space| position_taken?(board, space.to_i)}
+      false
+    end
+  end
 end
